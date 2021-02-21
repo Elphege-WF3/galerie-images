295e28043040425a1dd0e6c86c313bb5195268fd
Elphege-WF3 <elphege.renard@gmail.com>
2021-02-21 11:45:36 +0100

    modification titre

--- php/header.php
+++ php/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projet Galerie Images</title>
+    <title>Galerie Images</title>
     <link rel="stylesheet" href="./assets/css/style.css">
     <!-- COMMENTAIRE HTML -->
 </head>
